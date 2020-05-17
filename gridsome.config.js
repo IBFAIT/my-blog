a3c111b7f1cb3e894665d64616d881c20cef1cb0
Buck Bruno <buck@Bucks-MacBook-Pro.local>
2020-05-17 15:20:05 +0200

    first commit

---
+++ gridsome.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteName: 'My blog',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'articles/**/*.md',
+        typeName: 'Articles',
+        remark: {}
+      }
+    },
+  ],
+  transformers: {
+    remark: {}
+  }
+}
